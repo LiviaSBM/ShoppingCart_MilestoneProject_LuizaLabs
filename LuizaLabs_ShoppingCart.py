628102072a750ea7ba24249541bd398b4b6b182c
Livia Menezes <liviasbm@hotmail.com>
2022-08-31 19:38:28 -0400

    Code updated

--- LuizaLabs_ShoppingCart.py
+++ LuizaLabs_ShoppingCart.py
@@ -1,15 +1,18 @@
-cart = []
-# for i in range(3):
-id_user = input("Insira o id do usuário:")
-id_produto = input("Insira o id do produto:")
-price_product = float(input("Insira o preço do produto:"))
-quantity_product = int(input("Insira a quantidade de produto:"))
+shoppigcart = []
+
+diffproducts = int(input("How many different products would you like to buy?"))
+for i in range(diffproducts):
+    id_user = input("Insira o id do usuário:")
+    id_product = int(input("Insira o id do produto:"))
+    price_product = float(input("Insira o preço do produto:"))
+    quantity_product = int(input("Insira a quantidade de produto:"))
 
 
-item = [id_user, id_produto, price_product, quantity_product]
-
+item = [id_user, id_product, price_product, quantity_product]
+print(item)
 
 def add_item_cart(item):
+    #shoppingcart.append[item]
     pass
 
 def get_all_items_cart():
@@ -19,6 +22,6 @@ def get_all_items_cart():
 def get_item_cart_by_id(id_product):
     pass
 
-def remove_item_id(id_product)
+def remove_item_id(id_product):
     #remover o item do carrinho que tem esse produto
     pass
