96267846f44ee23aa2682b8cf08603f55ed18049
Livia Menezes <liviasbm@hotmail.com>
2022-09-01 18:18:56 -0400

    First module ready and functional

--- LuizaLabs_ShoppingCart.py
+++ LuizaLabs_ShoppingCart.py
@@ -1,27 +1,28 @@
-shoppigcart = []
+shoppingcart = []
+item = []
 
 diffproducts = int(input("How many different products would you like to buy?"))
 for i in range(diffproducts):
-    id_user = input("Insira o id do usuário:")
-    id_product = int(input("Insira o id do produto:"))
-    price_product = float(input("Insira o preço do produto:"))
-    quantity_product = int(input("Insira a quantidade de produto:"))
+    id_user = input("Insira o id do usuário: ")
+    productname = input("Insira o nome do produto: ")
+    id_product = int(input("Insira o id do produto: "))
+    price_product = float(input("Insira o preço do produto: "))
+    quantity_product = int(input("Insira a quantidade de produto: "))
+    
+    def add_item_cart(item):
+        item = [id_user, id_product, price_product, quantity_product]
+        shoppingcart.append(item)
+        return shoppingcart
+    
+def get_all_items_cart(shoppingcart):
+    newlist = [item[1] for item in shoppingcart]
+    return f"No carrinho, foram adicionados os seguintes itens: {newlist}"
 
-
-item = [id_user, id_product, price_product, quantity_product]
-print(item)
-
-def add_item_cart(item):
-    #shoppingcart.append[item]
-    pass
-
-def get_all_items_cart():
-    #return todos os itens do carrinho
-    pass
-
-def get_item_cart_by_id(id_product):
-    pass
+def get_item_cart_by_id(shoppingcart):
+    return [item[3] for item in shoppingcart]
 
 def remove_item_id(id_product):
     #remover o item do carrinho que tem esse produto
     pass
+
+get_all_items_cart(shoppingcart)
