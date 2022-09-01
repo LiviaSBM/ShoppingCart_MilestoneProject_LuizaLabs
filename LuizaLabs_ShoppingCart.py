c5732cd6dc0457cc468b0ee49c41b0eff1681449
Livia Menezes <liviasbm@hotmail.com>
2022-09-01 19:35:45 -0400

    Adding last module

--- LuizaLabs_ShoppingCart.py
+++ LuizaLabs_ShoppingCart.py
@@ -1,28 +1,53 @@
+diffproducts = int(input("How many different products would you like to buy?"))
 shoppingcart = []
 item = []
 
-diffproducts = int(input("How many different products would you like to buy?"))
-for i in range(diffproducts):
-    id_user = input("Insira o id do usuário: ")
-    productname = input("Insira o nome do produto: ")
-    id_product = int(input("Insira o id do produto: "))
-    price_product = float(input("Insira o preço do produto: "))
-    quantity_product = int(input("Insira a quantidade de produto: "))
+
+def add_item_cart(diffproducts):
     
-    def add_item_cart(item):
-        item = [id_user, id_product, price_product, quantity_product]
+    
+    for i in range(diffproducts):
+        id_user = input("Insira o id do usuário: ")
+        productname = input("Insira o nome do produto: ")
+        id_product = int(input("Insira o id do produto: "))
+        price_product = float(input("Insira o preço do produto: "))
+        quantity_product = int(input("Insira a quantidade de produto: "))
+    
+        item = [id_user, productname, id_product, price_product, quantity_product]
+    
         shoppingcart.append(item)
-        return shoppingcart
     
+    return shoppingcart
+
+shoppingcart=add_item_cart(diffproducts)
+#print(shoppingcart)
+
 def get_all_items_cart(shoppingcart):
     newlist = [item[1] for item in shoppingcart]
-    return f"No carrinho, foram adicionados os seguintes itens: {newlist}"
+    return f"No carrinho, foram adicionados os seguintes itens: {newlist}\n"
+
+print(get_all_items_cart(shoppingcart))
 
 def get_item_cart_by_id(shoppingcart):
-    return [item[3] for item in shoppingcart]
+    newlist = [item[2] for item in shoppingcart]
+    return f"Os IDs desses itens são: {newlist}\n"
 
-def remove_item_id(id_product):
-    #remover o item do carrinho que tem esse produto
-    pass
+print(get_item_cart_by_id(shoppingcart))
 
-get_all_items_cart(shoppingcart)
+def remove_item_id(diffproducts, shoppingcart):
+    removalquestion = input("Você gostaria de excluir algum item do carrinho? Escolha S/N \n")
+    dicrem = {}
+    while removalquestion=="s" or removalquestion=="S":
+        print("Segue abaixo a lista de produtos atualmente presentes no carrinho:\n")
+        for item in range(diffproducts):
+            print(f"Produto: {shoppingcart[item[1]]}\tID: {shoppingcart[item[2]]}")
+            dicrem[item[2]]=item
+            
+        itemtoberemoved = input("Digite o ID do item que gostaria de remover \n")
+        shoppingcart.pop(dicrem[itemtoberemoved])
+            
+        removalquestion = input("Você gostaria de excluir outro item? Escolha S/N \n")
+            
+    return f"The updated cart contains the following items: {shoppingcart}"
+
+#print(get_item_cart_by_id(shoppingcart))
